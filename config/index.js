25ead4c56ecdf4f0cc7f16a6daadbcd26f590b0f
James Gardiner <jamesgardiner075@gmail.com>
2017-03-17 16:30:02 +0000

    Add mongodb connection string

---
+++ config/index.js
@@ -0,0 +1,10 @@
+var configValues = require('./config');
+
+module.exports = {
+
+  getDbConnectString: function() {
+    return 'mongodb://' + configValues.uname + ':' + configValues.pwd +
+    '@ds055772.mlab.com:55772/movie-site';
+  }
+  
+};
