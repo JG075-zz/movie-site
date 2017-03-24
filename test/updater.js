8607749593f406ed5ed70dbdc80f06ac4babafa4
James Gardiner <jamesgardiner075@gmail.com>
2017-03-24 15:05:20 +0000

    Add method to get all records from db

--- test/updater.js
+++ test/updater.js
@@ -1,12 +1,24 @@
 var assert = require('chai').assert;
+var sinon = require('sinon');
 var Updater = require('../lib/updater');
-
+var mongoose = require('mongoose');
 var updater = new Updater();
 
 describe('Updater', function() {
 
   it('should instatiate a new object', function() {
     assert.isObject(updater);
+  });
+
+  describe('#updaterAll()', function () {
+
+    it('should connect to the database', function(done) {
+      updater.updateAll(function(){
+        assert.strictEqual(1, mongoose.connection.readyState);
+        done();
+      });
+    });
+
   });
 
 });
